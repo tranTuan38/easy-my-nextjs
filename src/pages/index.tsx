3b4f1f02466ae9d772ddebb3af5006f33edb4f78
tranTuan38 <tuine03082000@gmail.com>
2023-03-09 15:07:09 +0700

    update text dev

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,7 @@ export default function Home() {
     return (
         <div>
             <div className="relative w-full h-[600px] max-h-[600px]">
+                this is branch main
                 {/* <Image
                     alt="anh-nen"
                     src="/bg-img.jpg"
